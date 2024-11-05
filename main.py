08f696a0ced82168b014f1d1d4857d387fb573e9
Buyandy <kyzyloolbuandy9@gmail.com>
2024-11-05 16:58:12 +0700

    исправление проблем с цикл импортами

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 
 from DataStore.key_token import token
-from Tools import cust_json, all_parser
+from Tools import cust_json, parser
 
 from Handlers.greeting import router as greeting_router, storage
 from Handlers.tools import router as tools_router
@@ -28,7 +28,7 @@ async def main() -> None:
     dp.include_router(greeting_router)
     dp.include_router(tools_router)
 
-    asyncio.create_task(all_parser.auto_update_all_data())
+    asyncio.create_task(parser.auto_update_all_data())
     await launch_bot(cust_json.load_from_file("DataStore/users.json"), bot=bot)
     await dp.start_polling(bot)
 
